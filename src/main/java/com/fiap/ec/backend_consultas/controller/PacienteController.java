d861176935a80a727ad6cdd16edd17573996cc8a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-25 22:17:34 -0300

    Criado as exceptions

--- src/main/java/com/fiap/ec/backend_consultas/controller/PacienteController.java
+++ src/main/java/com/fiap/ec/backend_consultas/controller/PacienteController.java
@@ -2,15 +2,14 @@ package com.fiap.ec.backend_consultas.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fiap.ec.backend_consultas.exception.RecursoNaoEncontradoException;
 import com.fiap.ec.backend_consultas.model.Paciente;
 import com.fiap.ec.backend_consultas.service.PacienteService;
 @RestController
@@ -33,14 +32,9 @@ public class PacienteController {
     public Paciente buscarPorId(@PathVariable Long id) {
         return service.buscarPorId(id);
     }
-
-    @PutMapping("/{id}")
-    public Paciente atualizar(@PathVariable Long id, @RequestBody Paciente paciente) {
-        return service.atualizar(id, paciente);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deletar(@PathVariable Long id) {
-        service.deletar(id);
+    @GetMapping("/cpf/{cpf}")
+    public Paciente buscarPorCpf(@PathVariable String cpf) {
+        return service.buscarPorCpf(cpf)
+                .orElseThrow(() -> new RecursoNaoEncontradoException("CPF não encontrado."));
     }
 }
